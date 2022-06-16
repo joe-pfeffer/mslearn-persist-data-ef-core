d78dd10b984f08bde770bdb79bc36c3f4b2082c9
joe pfeffer <55728161+joe-pfeffer@users.noreply.github.com>
2022-06-16 12:20:07 -0600

    initial db updates

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,5 +1,5 @@
 using ContosoPizza.Services;
-// Additional using declarations
+using ContosoPizza.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
-
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
 
 builder.Services.AddScoped<PizzaService>();
