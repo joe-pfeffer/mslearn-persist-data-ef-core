66031690ccd4c88e51e7ff0dc496e0fed9c06219
joe pfeffer <55728161+joe-pfeffer@users.noreply.github.com>
2022-06-16 13:07:43 -0600

    modded db updates

--- ContosoPizza/Models/Sauce.cs
+++ ContosoPizza/Models/Sauce.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Sauce
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(100)]
+    [MinLength(2)]
     public string? Name { get; set; }
+
+    public bool IsVegan { get; set; }
 }
